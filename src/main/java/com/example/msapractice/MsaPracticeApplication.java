79ae53abf7dadb3db4050fbf1534cff7c1b6b445
dev-taeyeong <dev.taeyeong@gmail.com>
2023-10-03 06:21:01 +0900

    first commit

---
+++ src/main/java/com/example/msapractice/MsaPracticeApplication.java
@@ -0,0 +1,13 @@
+package com.example.msapractice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsaPracticeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MsaPracticeApplication.class, args);
+    }
+
+}
